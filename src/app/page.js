9beaaab819a0c4fe9050993c86434d565f7d445d
reddiedev <105657266+reddiedev@users.noreply.github.com>
2023-06-30 02:32:23 +0800

    added project image

--- src/app/page.js
+++ src/app/page.js
@@ -42,7 +42,7 @@ export default function Home() {
           </div>
         </nav>
       </header>
-      <section class="">
+      <section class="mt-20">
         <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div class="mr-auto place-self-center lg:col-span-7">
             <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
@@ -58,12 +58,13 @@ export default function Home() {
               occaecat enim.
             </p>
           </div>
-          <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
+          <div class="lg:mt-0 lg:col-span-5 p-10 flex items-center justify-center h-full">
             <Image
-              width={900}
-              height={700}
-              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png"
-              alt="mockup"
+              width={433}
+              height={577}
+              src="/setup.png"
+              className="object-contain w-80 "
+              alt="Project Setup"
             />
           </div>
         </div>
